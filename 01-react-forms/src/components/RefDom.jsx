c5cc177e5a5a7d0e1079646f10a90f84f41eb2c3
César Guerra <warderer@gmail.com>
2022-10-17 21:28:21 -0500

    ✨ ejempplo de useRef

---
+++ 01-react-forms/src/components/RefDom.jsx
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react'
+
+const RefDom = () => {
+  const inputRef = useRef()
+  console.log(inputRef.current)
+
+  useEffect(() => {
+    inputRef.current.focus()
+    inputRef.current.value = 'Hola Mundo'
+  }, [])
+
+  return (
+    <div>
+      <input ref={inputRef} type='text' />
+    </div>
+  )
+}
+export default RefDom
