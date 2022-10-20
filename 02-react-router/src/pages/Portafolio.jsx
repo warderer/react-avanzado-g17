c79cc6eb8853d0584250232f726bf05264bfccc7
César Guerra <warderer@gmail.com>
2022-10-19 21:31:53 -0500

    ✨ Ruteo Dinámico y Anidado con useParams y Outlet

--- 02-react-router/src/pages/Portafolio.jsx
+++ 02-react-router/src/pages/Portafolio.jsx
@@ -1,6 +1,25 @@
+import { Link, Outlet } from 'react-router-dom'
+
 const Portafolio = () => {
   return (
-    <h1>Portafolio</h1>
+    <>
+      <h1>Portafolio</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to='/portafolio/1'>Pinterest</Link>
+          </li>
+          <li>
+            <Link to='/portafolio/2'>Cajero</Link>
+          </li>
+          <li>
+            <Link to='/portafolio/3'>Pokédex</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
   )
 }
 export default Portafolio
