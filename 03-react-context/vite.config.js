09102cf668b86bdacc7984477affc48275af6578
Francisco Campos <hey@francampos.me>
2022-10-24 19:04:45 -0600

    Assets

--- 03-react-context/vite.config.js
+++ 03-react-context/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 1# añado el path
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // 2 Configuramos Alias @ para mis carpetas
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
