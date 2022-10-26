8c742531d598b22048595ff94c5f37f9891e5248
César Guerra <warderer@gmail.com>
2022-10-25 19:30:55 -0500

    ✨ Header con search bar, prev y next

--- 03-react-context/src/components/Header/Header.jsx
+++ 03-react-context/src/components/Header/Header.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { useSongContext } from '@/context/SongContext'
 
 const Header = () => {
+  const context = useSongContext()
+  const song = context.selectedSong
+
+  const prevSong = () => {
+    const songIndex = context.list.findIndex(s => s.id === song.id)
+    songIndex > 0
+      ? context.setSelectedSong(context.list[songIndex - 1])
+      : context.setSelectedSong(context.list[context.list.length - 1])
+  }
+
+  const nextSong = () => {
+    const songIndex = context.list.findIndex(s => s.id === song.id)
+    context.list.length > songIndex + 1
+      ? context.setSelectedSong(context.list[songIndex + 1])
+      : context.setSelectedSong(context.list[0])
+  }
+
+  const handleSearch = (event) => {
+    context.setSearch(event.target.value)
+  }
+
   return (
-    <div>Header</div>
+    song.title
+    ? (
+      <div>
+        Now Playing... {song.title} - {song.artist}
+        <input type='search' onChange={handleSearch} />
+        <button onClick={prevSong}> Prev </button>
+        <button onClick={nextSong}> Next </button>
+      </div>
+    )
+    : <div>Select a Song...</div>
   )
 }
 
--- 03-react-context/src/components/SongList/SongList.jsx
+++ 03-react-context/src/components/SongList/SongList.jsx
@@ -10,7 +10,14 @@ const SongList = () => {
       {
         context.loading
           ? <h1>Cargando...</h1>
-          : context.list.map((song) => (
+          : context.list.filter(song => {
+            if (context.search === '') {
+              return song // Si la busqueda es vacía, retorno todas las canciones
+            } else if (song.title.toLowerCase().includes(context.search.toLowerCase())) {
+              return song // Retorno la canción que cumple con el criterio de busqueda
+            }
+            return null // Ninguna canción coincide
+          }).map((song) => (
             <div className='row-song' key={song.id} onClick={() => { context.setSelectedSong(song) }}>
               <h4>{song.title}</h4>
               <p>{song.artist}</p>
