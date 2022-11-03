f8acabed4e7744272e77e49b3342506862a21520
Francisco Campos Diaz <franciscocamposdiaz@Franciscos-MacBook-Air.local>
2022-11-02 20:40:03 -0600

    header con context

--- 04-react-auth/src/components/Header/Header.jsx
+++ 04-react-auth/src/components/Header/Header.jsx
@@ -1,20 +1,42 @@
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthContext'
 import './header.scss'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+  const { isAuth, logout } = useContext(AuthContext)
+
   return (
     <nav className='header'>
       <Link to='/' className='header__logo'>LOGO</Link>
       <ul className='header__nav-list'>
-        <li className='header__list-item'>
-          <Link to='/secret' className='header__item-link  header__item-link--is-active'>Secret</Link>
-        </li>
-        <li className='header__list-item'>
-          <Link to='/login' className='header__item-link'>Login</Link>
-        </li>
-        <li className='header__list-item'>
-          <Link to='/signup' className='header__item-link'>Signup</Link>
-        </li>
+
+        {
+          !isAuth
+            ? (
+              <>
+                <li className='header__list-item'>
+                  <Link to='/login' className='header__item-link'>Login</Link>
+                </li>
+                <li className='header__list-item'>
+                  <Link to='/signup' className='header__item-link'>Signup</Link>
+                </li>
+              </>
+              )
+            : (
+              <>
+                <li className='header__list-item'>
+                  <Link to='/secret' className='header__item-link  header__item-link--is-active'>Secret</Link>
+                </li>
+                <li className='header__list-item'>
+                  <Link to='/' className='header__item-link  header__item-link--is-active' onClick={logout}>Logout</Link>
+                </li>
+
+              </>
+
+              )
+        }
+
       </ul>
     </nav>
   )
