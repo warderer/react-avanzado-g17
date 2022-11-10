4a6db29cbd494341935c4b56ee13b40fe64a5e0d
César Guerra <warderer@gmail.com>
2022-11-09 18:39:49 -0600

    🐛 Fix de JWT en primera llamada

--- 04-react-auth/src/services/userServices.js
+++ 04-react-auth/src/services/userServices.js
@@ -2,17 +2,27 @@ import axios from 'axios'
 
 const BASE_URL = 'https://ecomerce-master.herokuapp.com/api/v1'
 
-const token = window.localStorage.getItem('token')
+// Referencia: https://stackoverflow.com/questions/71989146/axios-instance-not-getting-the-token-from-local-storage-on-first-call-react-js
 
-const config = {
-  headers: {
-    Authorization: `JWT ${token}`
+axios.interceptors.request.use(
+  (config) => {
+    const user = window.localStorage.getItem('token')
+
+    if (user) {
+      config.headers.Authorization = `JWT ${user}`
+    }
+    console.log('request config headers', config.headers)
+    return config
+  },
+  (error) => {
+    // console.log("request error", error);
+    return Promise.reject(error)
   }
-}
+)
 
 const registerUserServices = (data) => axios.post(`${BASE_URL}/signup`, data)
 const loginUserServices = (data) => axios.post(`${BASE_URL}/login`, data)
-const getSingleUser = (id) => axios.get(`${BASE_URL}/user/${id}`, config)
+const getSingleUser = (id) => axios.get(`${BASE_URL}/user/${id}`)
 
 export {
   registerUserServices,
